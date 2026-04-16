635220a8029ef13ba44b3e2c01a5d02943e6c862
Dias <tiagomiguel082004gmail.com@MacBook-Air-de-Dias.local>
2026-04-16 19:51:16 +0100

    Update FormSubmission and FormTemplate models

--- app/Models/FormTemplate.php
+++ app/Models/FormTemplate.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class templates extends Model
+class FormTemplate extends Model
 {
     use HasFactory;
 
